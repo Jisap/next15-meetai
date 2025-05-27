aea73eb36499ea9fae7222eb78ddf9a8ca789540
Jorge Matos <jisapjisap@gmail.com>
2025-05-27 18:05:37 +0200

    Add authentication features with Better Auth integration
    - Integrated Better Auth for user authentication, including sign-up and login functionalities.
    - Updated `src/app/page.tsx` to include user input forms for registration and login.
    - Created API routes for authentication in `src/app/api/auth/[...all]/route.ts`.
    - Defined user and session schemas in `src/db/schema.ts` to support authentication.
    - Added `auth-client` and `auth` modules for managing authentication logic.
    - Updated `package.json` and `package-lock.json` to include `better-auth` and related dependencies.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,109 @@
+"use client"
+
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 
 export default function Home() {
+
+  const { data: session } = authClient.useSession()
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [name, setName] = useState("")
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email,
+      password,
+      name,
+    }, {
+      onRequest: (ctx) => {
+        //show loading
+      },
+      onSuccess: (ctx) => {
+        window.alert("User created successfully");
+      },
+      onError: (ctx) => {
+        window.alert("Something went wrong");
+      },
+    })
+  }
+
+  const onLogin = () => {
+    authClient.signIn.email({
+      email,
+      password
+    }, {
+      onRequest: (ctx) => {
+        //show loading
+      },
+      onSuccess: (ctx) => {
+        window.alert("User login successfully");
+      },
+      onError: (ctx) => {
+        window.alert("Something went wrong");
+      },
+    })
+  }
+
+  if (session) {
+    return (
+      <div className="p-4 flex flex-col gap-y-4 max-w-md mx-auto">
+        <h1>Welcome {session.user.name}</h1>
+
+        <Button onClick={() => authClient.signOut()}>
+          Sign Out
+        </Button>
+      </div>
+    )
+  }
+
   return (
-    <div className="text-4xl font-bold text-green-500">
-      <Button>
-        Click me
-      </Button>
+    <div className="p-4 flex flex-col gap-y-4 max-w-md mx-auto">
+      <div className="flex flex-col gap-y-4">
+        <Input
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={onSubmit}>
+          Create User
+        </Button>
+      </div>
+
+      <div className="flex flex-col gap-y-4">
+        <Input
+          placeholder="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={onLogin}>
+          Login
+        </Button>
+      </div>
     </div>
   );
 }
