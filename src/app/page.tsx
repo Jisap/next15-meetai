a4e0670cfef68d4fb1f26a5f6066b18b77da282e
Jorge Matos <jisapjisap@gmail.com>
2025-05-28 18:59:49 +0200

    Refactor authentication pages to utilize session management and improve navigation
    - Updated the main page to redirect unauthenticated users to the sign-in page.
    - Simplified the sign-in and sign-up pages by adding session checks and redirecting authenticated users to the home page.
    - Introduced a new `HomeView` component to display user information and sign-out functionality.
    - Removed unnecessary form handling from the main page, enhancing code clarity and maintainability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,109 +1,22 @@
-"use client"
+import { auth } from '@/lib/auth'
+import { HomeView } from '@/modules/home/ui/views/home-view'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+import React from 'react'
 
+const Page = async() => {
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { authClient } from "@/lib/auth-client";
-import { useState } from "react";
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
 
-
-export default function Home() {
-
-  const { data: session } = authClient.useSession()
-
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [name, setName] = useState("")
-
-  const onSubmit = () => {
-    authClient.signUp.email({
-      email,
-      password,
-      name,
-    }, {
-      onRequest: (ctx) => {
-        //show loading
-      },
-      onSuccess: (ctx) => {
-        window.alert("User created successfully");
-      },
-      onError: (ctx) => {
-        window.alert("Something went wrong");
-      },
-    })
-  }
-
-  const onLogin = () => {
-    authClient.signIn.email({
-      email,
-      password
-    }, {
-      onRequest: (ctx) => {
-        //show loading
-      },
-      onSuccess: (ctx) => {
-        window.alert("User login successfully");
-      },
-      onError: (ctx) => {
-        window.alert("Something went wrong");
-      },
-    })
-  }
-
-  if (session) {
-    return (
-      <div className="p-4 flex flex-col gap-y-4 max-w-md mx-auto">
-        <h1>Welcome {session.user.name}</h1>
-
-        <Button onClick={() => authClient.signOut()}>
-          Sign Out
-        </Button>
-      </div>
-    )
+  if(!session) {
+    redirect('/sign-in')
   }
 
   return (
-    <div className="p-4 flex flex-col gap-y-4 max-w-md mx-auto">
-      <div className="flex flex-col gap-y-4">
-        <Input
-          placeholder="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <Input
-          placeholder="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          placeholder="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button onClick={onSubmit}>
-          Create User
-        </Button>
-      </div>
-
-      <div className="flex flex-col gap-y-4">
-        <Input
-          placeholder="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          placeholder="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button onClick={onLogin}>
-          Login
-        </Button>
-      </div>
-    </div>
-  );
+    <HomeView />
+  )
 }
+
+export default Page
