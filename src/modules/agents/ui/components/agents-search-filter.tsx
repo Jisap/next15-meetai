8b4091bc771786818ff9e67788a7be89755f781b
Jorge Matos <jisapjisap@gmail.com>
2025-06-02 19:59:47 +0200

    Enhance agent list header with search filter and clear functionality
    - Updated `AgentListHeader` to include `AgentsSearchFilter` for improved agent name filtering.
    - Added a clear filters button that resets the search input and pagination to default values when filters are modified.
    - Renamed `SearchFilter` component to `AgentsSearchFilter` for better clarity and consistency.

--- src/modules/agents/ui/components/agents-search-filter.tsx
+++ src/modules/agents/ui/components/agents-search-filter.tsx
@@ -4,8 +4,8 @@ import { useAgentsFilter } from "../../hooks/use-agents-filter";
 
 
 
-export const SearchFilter = () => {
-  const [filters, setFilters] = useAgentsFilter();
+export const AgentsSearchFilter = () => {
+  const [filters, setFilters] = useAgentsFilter(); // 
 
   return(
     <div className="relative">
@@ -13,7 +13,7 @@ export const SearchFilter = () => {
         placeholder="Filter by name..."
         className="h-9 bg-white w-[200px] pl-7"
         value={filters.search}
-        onChange={(e) => setFilters({ search: e.target.value })}
+        onChange={(e) => setFilters({ search: e.target.value })} // Input -> setFilters -> url modificada con ?search=value 
       />
       <SearchIcon className="size-4 absolute left-2 top-1/2 -translate-y-1/2 text-muted-foreground"/>
     </div>
