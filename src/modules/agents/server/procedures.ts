a272a9f17a03839d20294c6fd047069fe50aec1d
Jorge Matos <jisapjisap@gmail.com>
2025-06-02 13:47:58 +0200

    comments

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -29,47 +29,47 @@ export const agentsRouter = createTRPCRouter({
   }),
 
   getMany: protectedProcedure
-    .input( // Filters
+    .input(                                                             // Filters
       z.object({
-        page: z.number().min(1).default(DEFAULT_PAGE),
-        pageSize: z
+        page: z.number().min(1).default(DEFAULT_PAGE),                  // Se espera un número de página
+        pageSize: z                                                     // Se espera un tamaño de página
           .number()
           .min(MIN_PAGE_SIZE)
           .max(MAX_PAGE_SIZE)
           .default(DEFAULT_PAGE_SIZE),
-        search: z.string().nullish(),
+        search: z.string().nullish(),                                   // Se espera un termino de búsqueda
       })
     )
-    .query(async({ ctx, input}) => {
+    .query(async({ ctx, input}) => {                                    // Consulta recibiendo los filtros y el ctx con el usuario autenticado
       const { search, page, pageSize } = input;
-      const data = await db
-        .select({
+      const data = await db                                             // Se obtienen los datos de la tabla agents
+        .select({                                                       // Para ello se seleccionan las columnas de la tabla agents
           // TODO: Change to actual count
-          meetingCount: sql<number>`5`,   // Se agrega una columna de tipo number llamada meetingCount
-          ...getTableColumns(agents),     // Se seleccionan todas las columnas de la tabla agents
+          meetingCount: sql<number>`5`,                                 // Se agrega una columna de tipo number llamada meetingCount
+          ...getTableColumns(agents),                                   // Se seleccionan todas las columnas de la tabla agents
         })
         .from(agents)
         .where(
           and(
-            eq(agents.userId, ctx.auth.user.id),
-            search ? ilike(agents.name, `%${search}%`) : undefined
+            eq(agents.userId, ctx.auth.user.id),                        // Filtra los "agents" para que solo se devuelvan aquellos cuyo userId coincida con el ID del usuario autenticado
+            search ? ilike(agents.name, `%${search}%`) : undefined      // Usamos el operador de comparación ilike para buscar por nombres de agents que contienen la palabra de búsqueda
           )
         )
-        .orderBy(desc(agents.createdAt), desc(agents.id))
-        .limit(pageSize)
-        .offset((page - 1) * pageSize)
+        .orderBy(desc(agents.createdAt), desc(agents.id))               // Ordena los resultados de forma descendente
+        .limit(pageSize)                                                // Limita el número de resultados devueltos al pageSize especificado.
+        .offset((page - 1) * pageSize)                                  // Omite un número de resultados para implementar la paginación. Por ejemplo, si page es 2 y pageSize es 10, se omitirán los primeros (2-1)*10 = 10 resultados. 
 
-      const [total] = await db
-        .select({ count: count() })
+      const [total] = await db                                          // Segunda Consulta a la Base de Datos (Obtener Total de Elementos)                                  
+        .select({ count: count() })                                     // count() cuenta el número de filas que coinciden con la clausula where
         .from(agents)
         .where(
           and(
-            eq(agents.userId, ctx.auth.user.id),
-            search ? ilike(agents.name, `%${search}%`) : undefined
+            eq(agents.userId, ctx.auth.user.id),                        // Agentes que pertenecen al usuario autenticado
+            search ? ilike(agents.name, `%${search}%`) : undefined      // Agentes cuyo name contiene la palabra de búsqueda
           )
         )
 
-        const totalPages = Math.ceil(total.count / pageSize);
+      const totalPages = Math.ceil(total.count / pageSize);             // Calcula el número total de páginas necesarias para mostrar todos los elementos, dividiendo el conteo total por el tamaño de página y redondeando hacia arriba  
   
         return {
           items: data,
