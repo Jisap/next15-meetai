aed28ee1849b3e403cb779a865981623127d3d72
Jorge Matos <jisapjisap@gmail.com>
2025-06-03 18:52:24 +0200

    Add agent update and removal procedures with validation
    - Introduced `agentsUpdateSchema` for validating agent updates, ensuring required fields are present.
    - Added a `remove` procedure in the `agentsRouter` to handle agent deletion, including user authentication checks and error handling for non-existent agents.
    - Enhanced the overall agent management functionality by implementing robust validation and error responses.

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -4,7 +4,6 @@ import { db } from "@/db";
 import { agentsInsertSchema } from "../schemas";
 import { z } from "zod";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
-import page from "@/app/(dashboard)/meetings/page";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
 
@@ -12,6 +11,25 @@ import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
  
+  remove: protectedProcedure
+    .input( z.object({ id: z.string() }))
+    .mutation(async({ input, ctx }) => {
+      const [removedAgent] = await db
+      .delete(agents)
+      .where(
+        and(
+          eq(agents.id, input.id),
+          eq(agents.userId, ctx.auth.user.id)
+        )
+      )
+      .returning()
+      
+      if(!!removedAgent){
+        throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" })
+      }
+
+      return removedAgent;
+  }),
   getOne: protectedProcedure
     .input( z.object({ id: z.string() }))
     .query(async({ input, ctx }) => {
