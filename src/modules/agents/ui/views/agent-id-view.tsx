7e09886e07992357feac1df8995e8c6164dd97af
Jorge Matos <jisapjisap@gmail.com>
2025-06-04 15:39:07 +0200

    Add confirmation dialog hook for agent removal functionality
    - Introduced a new `useConfirm` hook to manage confirmation dialogs, enhancing user interaction when removing agents.
    - Integrated the `useConfirm` hook into the `AgentIdView` component, allowing for confirmation before agent removal.
    - Improved user experience by providing clear confirmation prompts with customizable titles and descriptions.

--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -11,6 +11,7 @@ import { Badge } from '@/components/ui/badge';
 import { VideoIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
+import { useConfirm } from '../../hooks/use-confirm';
 
 
 interface Props {
@@ -44,50 +45,67 @@ export const AgentIdView = ({ agentId }: Props) => {
         toast.error(error.message);
       }
     })
-  )
+  );
+
+  //       ConfirmationDialog, confirm
+  const [RemovedConfirmation, confirmRemove] = useConfirm(
+    "Are you sure?",
+    `The following action will remove ${data.meetingCount} associated meeting`
+  );
+
+  const handleRemoveAgent = async () => {           // Cuando se pulsa el botón de remover
+    const ok = await confirmRemove()                // Se crea la promesa de confirmación -> se abre el dialogo
+
+    if(!ok) return                                  // Se se le da a cancel => promise=false y se cierra el dialogo
+
+    await removeAgent.mutateAsync({ id: agentId })  // Si se se le da a confirm => promise=true -> mutation y se cierra el dialogo
+  }
 
   return (
-    <div className='flex-1 py-4 md:px-8 flex flex-col gap-y-4'>
-      <AgentIdViewHeader
-        agentId={agentId}
-        agentName={data.name}
-        onEdit={() => { }}
-        onRemove={() => removeAgent.mutate({ id: agentId })}
-      />
+    <>
+      <RemovedConfirmation />
+      <div className='flex-1 py-4 md:px-8 flex flex-col gap-y-4'>
+        <AgentIdViewHeader
+          agentId={agentId}
+          agentName={data.name}
+          onEdit={() => { }}
+          onRemove={handleRemoveAgent}
+        />
 
-      <div className='bg-white rounded-lg border'>
-        <div className='px-4 py-5 gap-y-5 flex flex-col col-span-5'>
-          <div className='flex items-center gap-x-3'>
-            <GeneratedAvatar
-              variant="botttsNeutral"
-              seed={data.name}
-              className='size-10'
-            />
+        <div className='bg-white rounded-lg border'>
+          <div className='px-4 py-5 gap-y-5 flex flex-col col-span-5'>
+            <div className='flex items-center gap-x-3'>
+              <GeneratedAvatar
+                variant="botttsNeutral"
+                seed={data.name}
+                className='size-10'
+              />
 
-            <h2 className='text-2xl font-medium'>
-              {data.name}
-            </h2>
-          </div>
+              <h2 className='text-2xl font-medium'>
+                {data.name}
+              </h2>
+            </div>
 
-          <Badge
-            variant="outline"
-            className='flex items-center gap-x-2 [&>svg]:size-4'
-          >
-            <VideoIcon className='text-blue-700'/>
-            {data.meetingCount} {data.meetingCount === 1 ? 'Meeting' : 'Meetings'}
-          </Badge>
+            <Badge
+              variant="outline"
+              className='flex items-center gap-x-2 [&>svg]:size-4'
+            >
+              <VideoIcon className='text-blue-700'/>
+              {data.meetingCount} {data.meetingCount === 1 ? 'Meeting' : 'Meetings'}
+            </Badge>
 
-          <div className='flex flex-col gap-y-4'>
-            <p className='text-lg font-medium'>
-              Instructions
-            </p>
-            <p className='text-neutral-800'>
-              {data.instructions}
-            </p>
+            <div className='flex flex-col gap-y-4'>
+              <p className='text-lg font-medium'>
+                Instructions
+              </p>
+              <p className='text-neutral-800'>
+                {data.instructions}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
