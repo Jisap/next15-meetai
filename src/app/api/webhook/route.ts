b414852afd0d09fdb772f42e48363f3e086dd541
Jorge Matos <jisapjisap@gmail.com>
2025-06-12 16:34:14 +0200

    Add webhook route for Stream Video event handling
    - Introduced a new API route to handle incoming webhook events from Stream Video, including call session management and verification of signatures.
    - Implemented logic to update meeting statuses based on event types, ensuring accurate tracking of call states.
    - Integrated database interactions to manage meetings and agents associated with the video calls.
    - Added error handling for missing or invalid signatures and payloads to enhance robustness.

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,127 @@
+
+import { and, eq, not } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+import {
+  CallEndedEvent,
+  MessageNewEvent,
+  CallTranscriptionReadyEvent,
+  CallSessionParticipantLeftEvent,
+  CallRecordingReadyEvent,
+  CallSessionStartedEvent,
+} from "@stream-io/node-sdk";
+import { db } from "@/db";
+import { agents, meetings } from '../../../db/schema';
+import { streamVideo } from "@/lib/stream-video";
+
+function verifySignatureWithSDK(                    // Comprueba si la solicitud realmente proviene de Stream y no ha sido manipulada
+  body: string,                                     // Recibe el cuerpo de la solicitud y                   
+  signature: string,                                // la firma que Steam envía en las cabeceras
+): boolean {
+  return streamVideo.verifyWebhook(body, signature) // verifywbhook valida si la firma es válida (true)
+}
+
+
+// Manejador de eventos de Stream Video. Se ejecuta cuando Stream envía una solicitud POST a la url /api/webhook
+
+export async function POST(req: NextRequest){
+
+  const signature = req.headers.get("x-signature");    // Obtiene la firma de la solicitud
+  const apikey = req.headers.get("x-api-key");         // Obtiene la clave de API de Stream
+
+  if(!signature || !apikey){
+    return NextResponse.json(
+      { error: "Missing signature or apikey" },
+      { status: 400 }
+    )
+  }
+
+  const body = await req.text();                       // Obtiene el cuerpo de la solicitud como texto plano porque la verificación se basa en el contenido exacto
+
+
+  if(!verifySignatureWithSDK(body, signature)){        // Verifica si la firma es válida
+    return NextResponse.json(
+      { error: "Invalid signature" },
+      { status: 401 }
+    )
+  }
+
+  let payload: unknown;
+
+  try{
+    payload = JSON.parse(body) as Record<string, unknown>; // Se parsea el cuerpo de la solicitud (que es una cadena de texto con formato JSON) para convertirlo en un objeto JavaScript.
+  }catch{
+    return NextResponse.json(
+      { error: "Invalid payload" },
+      { status: 400 }
+    )
+  }
+
+  const eventType = (payload as Record<string, unknown>)?.type; // Extrae la prop type del objeto payload. type nos indica que tipo de evento ha ocurrido
+
+  if(eventType === "call.session_started"){                     // Si el evento es call.session_started 
+    const event = payload as CallSessionStartedEvent;           // se convierte el payload en un objeto de tipo CallSessionStartedEvent para acceder a sus propiedades de forma segura
+    const meetingId = event.call.custom?.meetingId;             // Se extrae el meetingId personalizado que se estableció al crear la llamada
+
+    if(!meetingId){
+      return NextResponse.json(
+        { error: "Missing meetingId" },
+        { status: 400 }
+      )
+    }
+
+    const [existingMeeting] = await db                           // Se busca en la base de datos si existe un meeting con el id especificado
+      .select()
+      .from(meetings)
+      .where(
+        and(
+          eq(meetings.id, meetingId),
+          not(eq(meetings.status, "completed")),
+          not(eq(meetings.status, "active")),
+          not(eq(meetings.status, "cancelled")),
+          not(eq(meetings.status, "processing")),
+        )
+      )
+      
+
+    if(!existingMeeting){
+      return NextResponse.json(
+        { error: "Meeting not found" },
+        { status: 404 }
+      )
+    }
+
+    await db                                                      // Se actualiza el estado de la reunión en la base de datos
+      .update(meetings)
+      .set({
+        status: "active",
+        startedAt: new Date(),
+      })
+      .where(eq(meetings.id, existingMeeting.id))
+
+    const[existingAgent] = await db                                // Se busca en tabla agents si existe un agente asociado al meeting
+      .select()
+      .from(agents)
+      .where(eq(agents.id, existingMeeting.agentId))
+    
+    if(!existingAgent){
+      return NextResponse.json(
+        { error: "Agent not found" },
+        { status: 404 }
+      )
+    }
+
+    const call = streamVideo.video.call("default", meetingId)       // Se crea una nueva llamada de stream video con el id del meeting
+
+    const realtimeClient = await streamVideo.video.connectOpenAi({
+      call,
+      openAiApiKey: process.env.GEMINI_API_KEY!,
+      agentUserId: existingAgent.id,
+    })
+
+    realtimeClient.updateSession({
+      instructions: existingAgent.instructions
+    })
+  }
+
+  return NextResponse.json({ status: "ok" });
+}
