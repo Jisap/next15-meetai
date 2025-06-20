cfe2188e7b2cbda25eb5d3611bbd74524b4a4521
Jorge Matos <jisapjisap@gmail.com>
2025-06-20 18:03:06 +0200

    Add PricingCard component and enhance UpgradeView with subscription details
    - Introduced a new `PricingCard` component for displaying subscription options, featuring customizable styles and a button for user actions.
    - Updated `UpgradeView` to fetch and display the current subscription plan, improving user experience by clearly indicating the user's subscription status.

--- src/modules/premium/ui/views/upgrade-view.tsx
+++ src/modules/premium/ui/views/upgrade-view.tsx
@@ -5,6 +5,7 @@ import { LoadingState } from "@/components/loading-state"
 import { authClient } from "@/lib/auth-client"
 import { useTRPC } from "@/trpc/client"
 import { useSuspenseQuery } from "@tanstack/react-query"
+import { trpc } from '../../../../trpc/server';
 
 export const UpgradeViewLoading = () => {
   return (
@@ -23,9 +24,26 @@ export const UpgradeViewError = () => {
 }
 
 export const UpgradeView = () => {
+
+  const trpc = useTRPC();
+  const baseQueryOptions = trpc.premium.getCurrentSubscription.queryOptions();
+  const { data: currentSubscription } = useSuspenseQuery({                      // Por defecto los usuarios tienen un plan free
+    ...baseQueryOptions,
+    retry: 3,
+  })
+
+
   return (
-    <div>
-      UpgradeView
+    <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-10">
+      <div className="mt-4 flex-1 flex flex-col gap-y-10 items-center">
+        <h5 className="font-medium text-3xl md:text-3xl">
+          You are on the{" "}
+          <span className="font-semibold text-primary">
+            {currentSubscription?.name ?? "Free"}
+          </span>{" "}
+          plan
+        </h5>
+      </div>
     </div>
   )
 }
